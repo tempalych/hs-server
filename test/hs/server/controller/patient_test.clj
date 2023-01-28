63bfc98748aa03522130da007c99d002f3c88212
artem <wucweq-kovcuk-fezKe3>
2023-01-28 17:52:39 +0100

    try with broken tests

--- test/hs/server/controller/patient_test.clj
+++ test/hs/server/controller/patient_test.clj
@@ -91,7 +91,7 @@
          {:status 200
           :headers {"content-type" "application/json"}
           :body (json/encode '({:id 1
-                                :fname "fname1"
+                                :fname "fname"
                                 :lname "lname1"
                                 :pname "pname1"
                                 :address "address1"
